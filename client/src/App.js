dd2ac2bf8ff40dab1b2e88b1aac8b6585114ea9d
Jake Afuvai <jtafuvai@gmail.com>
2019-06-12 12:15:23 -0600

    i like these changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      Changess
-=======
       Changess Jake
       Changessss
->>>>>>> 426f96151eea732e50c7689f57e164c9baaefd47
     </div>
   );
 }
