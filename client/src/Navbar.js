d0212676a2fd2ef4d13f18c1fd7f4fbaa2482658
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-06-13 15:25:10 -0600

    navbarReactBurgerMenu

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,6 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-const Navbar = () => {
+// const Navbar = () => {
+// 	return (
+// 		<header className='navbar__container'>
+// 			<nav className='navbar'>
+// 				<ul classname='navbar__menu'>
+// 					<li className='navbar__item'><a className='nav__link' href='#'>Home</a></li>
+// 					<li className='navbar__item'><a className='nav__link' href='#'>Menu</a></li>
+// 					<li className='navbar__item'><a className='nav__link' href='#'>About</a></li>
+// 					<li className='navbar__item'><a className='nav__link' href='#'>Contact</a></li>
+// 					<li className='navbar__item'><a className='nav__link' href='#'>Directions</a></li>
+// 				</ul>
+// 			</nav>
+// 		</header>
+// 	)
+// }
+
+// export default Navbar;
+
+import {bubble as Menu}  from 'react-burger-menu'
+class Navbar extends React.Component{
+	showSettings (event){
+		event.preventDefault()
+	}
+
+render(){
 	return (
 		<header className='navbar__container'>
 			<nav className='navbar'>
@@ -10,10 +34,12 @@ const Navbar = () => {
 					<li className='navbar__item'><a className='nav__link' href='#'>About</a></li>
 					<li className='navbar__item'><a className='nav__link' href='#'>Contact</a></li>
 					<li className='navbar__item'><a className='nav__link' href='#'>Directions</a></li>
+					<li className= 'navbar__item'><a onClick={this.showSettings} className='navbar__link' href='' >Settings</a></li>
 				</ul>
 			</nav>
 		</header>
+		
 	)
+	
+	}
 }
-
-export default Navbar;
