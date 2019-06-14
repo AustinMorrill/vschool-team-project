6062e23f057233b2cbbb02385b029bd3da2253b4
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-06-14 09:53:24 -0600

    newnavbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,32 +1,27 @@
-// import React from 'react';
+import React from 'react';
 
-// const Navbar = () => {
-// 	return (
-// 		<header className='navbar__container'>
-// 			<nav className='navbar'>
-// 				<ul classname='navbar__menu'>
-// 					<li className='navbar__item'><a className='nav__link' href='#'>Home</a></li>
-// 					<li className='navbar__item'><a className='nav__link' href='#'>Menu</a></li>
-// 					<li className='navbar__item'><a className='nav__link' href='#'>About</a></li>
-// 					<li className='navbar__item'><a className='nav__link' href='#'>Contact</a></li>
-// 					<li className='navbar__item'><a className='nav__link' href='#'>Directions</a></li>
-// 				</ul>
-// 			</nav>
-// 		</header>
-// 	)
-// }
-
-// export default Navbar;
-
-import {bubble as Menu}  from 'react-burger-menu'
 class Navbar extends React.Component{
-	showSettings (event){
-		event.preventDefault()
+	constructor(){
+		super()
+		this.state={
+			isOpen:true
+		}
+
+
+
+		toggleMenu=()=>{
+			this.setState({isOpen : !state.isOpen})
+		}
 	}
 
-render(){
+		render(){
 	return (
 		<header className='navbar__container'>
+			<div className='hamburger' onClick={this.toggleMenu}>
+				<span className = 'line'></span>
+				<span className = 'line'></span>
+				<span className = 'line'></span>
+			</div>
 			<nav className='navbar'>
 				<ul classname='navbar__menu'>
 					<li className='navbar__item'><a className='nav__link' href='#'>Home</a></li>
@@ -34,12 +29,53 @@ render(){
 					<li className='navbar__item'><a className='nav__link' href='#'>About</a></li>
 					<li className='navbar__item'><a className='nav__link' href='#'>Contact</a></li>
 					<li className='navbar__item'><a className='nav__link' href='#'>Directions</a></li>
-					<li className= 'navbar__item'><a onClick={this.showSettings} className='navbar__link' href='' >Settings</a></li>
 				</ul>
 			</nav>
-		</header>
-		
+		</header> 
 	)
-	
-	}
+		}
+
 }
+export default Navbar;
+
+// import {bubble as Menu}  from 'react-burger-menu'
+// class Navbar extends React.Component{
+// 	constructor(){
+// 		super()
+// 		this.state = {
+// 			isOpen : true
+// 		}
+		
+// 	}
+// 	showSettings (event){
+// 		event.preventDefault()
+// 	}
+// 	handleChange(){
+// 		this.setState({isOpen : state.isOpen})
+
+// 	}
+// 	toggleMenu(){
+// 		this.setState(state => !state.isOpen)
+// 	}
+	
+
+// render(){
+// 	return (
+// 	      <div>
+// 			<Menu className = 'navbar' 
+// 			      onChenge = {this.handleChange} >
+				
+// 					<a className='nav__link' href='#'>Home</a>
+// 					<a className='nav__link' href='#'>Menu</a>
+// 					<a className='nav__link' href='#'>About</a>
+// 					<a className='nav__link' href='#'>Contact</a>
+// 					<a className='nav__link' href='#'>Directions</a>
+// 					<a onClick={this.showSettings} className='navbar__link' href='' >Settings</a>
+				
+// 			</Menu>
+// 			</div>
+
+// 	)
+	
+// 	}
+// }
