a5f96b6530fc3df636a544cfe395bcd7af954c47
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-06-17 09:28:39 -0600

    working navbar v1 and new libraries

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,81 +1,69 @@
-import React from 'react';
-
-class Navbar extends React.Component{
-	constructor(){
-		super()
-		this.state={
-			isOpen:true
-		}
+import React,{Component} from 'react';
+import HamburgerBtn from './HamburgerBtn'
+import './App.css'
 
 
 
-		toggleMenu=()=>{
-			this.setState({isOpen : !state.isOpen})
-		}
-	}
 
-		render(){
-	return (
-		<header className='navbar__container'>
-			<div className='hamburger' onClick={this.toggleMenu}>
-				<span className = 'line'></span>
-				<span className = 'line'></span>
-				<span className = 'line'></span>
+
+// sticky navbar 
+
+	// this function just to check if the page scrolled or not 
+	// componentDidMount(){
+	// 	window.addEventListener('scroll', ()=>{
+	// 		const isTop = window.scrollY < 100
+	// 		if (isTop !== true){
+	// 			{this.setState ({scrolled : true})}
+	// 		}else {
+	// 			{this.setState ({scrolled : false})}
+	// 		}
+	// 	})
+
+	// }
+
+	// componentWillUnmount(){
+	// 	window.removeEventListener('scroll')
+
+	// }
+
+    // hamburgerBtnClickHandler=()=>{
+	// this.setState((prevState)=>{
+	//  return {sideDrawerOpen: !prevState.sideDrawerOpen}
+	// })
+	
+
+
+const Navbar =(props)=>{
+
+return(
+		
+			//{this.state.scrolled ? 'navbar' : 'navbar_scrolled'
+			<div>
+			<header className='navbar__container'>
+			<nav className= 'navbar'> 
+				{/* <nav className= {this.state.scrolled ? 'navbar_scrolled' : 'navbar'}> */}
+				<div>
+                 <HamburgerBtn click = {props.hamburgerClickHandler} cross={props.corssHandler}/>
+				</div>
+				  <div className='navegation'>
+					<a className='nav__link' href='#'>Home</a>
+				    <a className='nav__link' href='#'>Menu</a>
+					<a className='nav__link' href='#'>Contact</a>
+					<a className='nav__link' href='#'>Directions</a>
+			     </div>
+				</nav>
+			</header> 
 			</div>
-			<nav className='navbar'>
-				<ul classname='navbar__menu'>
-					<li className='navbar__item'><a className='nav__link' href='#'>Home</a></li>
-					<li className='navbar__item'><a className='nav__link' href='#'>Menu</a></li>
-					<li className='navbar__item'><a className='nav__link' href='#'>About</a></li>
-					<li className='navbar__item'><a className='nav__link' href='#'>Contact</a></li>
-					<li className='navbar__item'><a className='nav__link' href='#'>Directions</a></li>
-				</ul>
-			</nav>
-		</header> 
-	)
-		}
+		
+)
 
 }
+
+
 export default Navbar;
 
-// import {bubble as Menu}  from 'react-burger-menu'
-// class Navbar extends React.Component{
-// 	constructor(){
-// 		super()
-// 		this.state = {
-// 			isOpen : true
-// 		}
-		
-// 	}
-// 	showSettings (event){
-// 		event.preventDefault()
-// 	}
-// 	handleChange(){
-// 		this.setState({isOpen : state.isOpen})
 
-// 	}
-// 	toggleMenu(){
-// 		this.setState(state => !state.isOpen)
-// 	}
-	
 
-// render(){
-// 	return (
-// 	      <div>
-// 			<Menu className = 'navbar' 
-// 			      onChenge = {this.handleChange} >
-				
-// 					<a className='nav__link' href='#'>Home</a>
-// 					<a className='nav__link' href='#'>Menu</a>
-// 					<a className='nav__link' href='#'>About</a>
-// 					<a className='nav__link' href='#'>Contact</a>
-// 					<a className='nav__link' href='#'>Directions</a>
-// 					<a onClick={this.showSettings} className='navbar__link' href='' >Settings</a>
-				
-// 			</Menu>
-// 			</div>
 
-// 	)
-	
-// 	}
-// }
+
+
